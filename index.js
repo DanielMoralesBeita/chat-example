05ef63e5148c255d41429e074bb7a69532f7c778
Daniel Alberto Morales Beita <Dani9milpz@gmail.com>
2018-09-21 16:25:13 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var port = process.env.PORT || 8000;
+ 
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -11,12 +13,7 @@ io.on('connection', function(socket){
     io.emit('chat message', msg);
   });
 });
-var port = process.env.PORT || 8000;
- 
-
 server.listen(port, function() {
     console.log("App is running on port " + port);
 });
-/*http.listen(2000, function(){
-  console.log('listening on web *:2000');
-});*/
+ 
